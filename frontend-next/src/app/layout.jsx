a0693c1f4a4c3b1ba7498f4e9661fa601b8a2201
ofekBinder <ofekBinder@gmail.com>
2024-07-06 21:12:04 +0300

    nav bar

--- frontend-next/src/app/layout.jsx
+++ frontend-next/src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 // import "./globals.css";
+import Navbar from "./components/Navbar"; // ייבוא רכיב ה-Navbar
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+        <Navbar />
+        <body className={inter.className}>{children}</body>
     </html>
   );
 }
