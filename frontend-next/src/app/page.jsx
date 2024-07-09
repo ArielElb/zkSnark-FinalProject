8810213856b157e468d67c92628ab0db97e40f44
arie1 <ariel123elbaz@gmail.com>
2024-07-09 18:23:46 +0300

    css and home page

--- frontend-next/src/app/page.jsx
+++ frontend-next/src/app/page.jsx
@@ -1,138 +1,128 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
       <div className={styles.container}>
         <header className={styles.header}>
+          <div className={styles.headerParagraph}>
+            <h1>Home Page</h1>
+            <p>
+              Zero-Knowledge Proofs (zkProofs) are cryptographic protocols that allow one
+              party (the prover) to prove to another party (the verifier) that they know a
+              value or statement without revealing any information about the value itself.
+              This project leverages the SP1 framework and{" "}
+              <a href='https://arkworks.rs/' target='_blank' rel='noopener noreferrer'>
+                arkworks.rs
+              </a>{" "}
+              to implement zkProofs for various computational tasks, including:
+            </p>
+            <ul>
+              <li>
+                <strong>Fermat Primality Test:</strong> We are using arkworks.rs to prove
+                that given a seed x for the randomness,and another number i, we can prove
+                that we compute and check if n=hash(x+i) is prime. (i.e., the hash of the
+                sum of x and i is a prime number) when the randomness for the fermat
+                primality test is a witness r = hash(x+i || n || i )
+              </li>
+              <li>
+                <strong>Matrix Multiplication Verification:</strong> Demonstrating the
+                integrity of matrix computations and their results using arkworks.rs,
+                without revealing the matrices themselves.
+              </li>
+              <li>
+                <strong>Fibonacci Sequence Computation:</strong> Showing how arkworks.rs
+                can ensure the accuracy of computed Fibonacci numbers while keeping the
+                initial parameters private.
+              </li>
+            </ul>
 
-        <div className={styles.headerParagraph}>
-        <h1>Home Page</h1>
-          <p>
-            Zero-Knowledge Proofs (zkProofs) are cryptographic protocols that allow one
-            party (the prover) to prove to another party (the verifier) that they know a
-            value or statement without revealing any information about the value itself.
-            This project leverages the SP1 framework and{" "}
-            <a href='https://arkworks.rs/' target='_blank' rel='noopener noreferrer'>
-              arkworks.rs
-            </a>{" "}
-            to implement zkProofs for various computational tasks, including:
-          </p>
-          <ul>
-            <li>
-              <strong>Miller-Rabin Primality Test:</strong> Using SP1, we verify the
-              correctness of prime tests without disclosing the number itself.
-            </li>
-            <li>
-              <strong>Matrix Multiplication Verification:</strong> Demonstrating the
-              integrity of matrix computations and their results using arkworks.rs,
-              without revealing the matrices themselves.
-            </li>
-            <li>
-              <strong>Fibonacci Sequence Computation:</strong> Showing how arkworks.rs can
-              ensure the accuracy of computed Fibonacci numbers while keeping the initial
-              parameters private.
-            </li>
-          </ul>
-          <p>
-            The Next.js web front-end provides an intuitive interface for users to
-            interact with these zkProofs, demonstrating the practical application of
-            cryptographic protocols in a user-friendly environment.
-          </p>
-          <p>
-            <b>Cryptographic Primitives: </b> Utilizing the Poseidon sponge hash function
-            for matrix multiplication and SHA-256 for the primality test, ensuring robust
-            security and efficiency in our proofs.
-          </p>
-          <h2>Key Concepts Explained:</h2>
-          <h3>1. What is zkSNARK?</h3>
-          <p>
-            Zero-Knowledge Succinct Non-Interactive Argument of Knowledge (zkSNARK) is a
-            cryptographic protocol that enables efficient verification of computations
-            without revealing private data. It allows a prover to convince a verifier that
-            a statement is true without divulging any additional information beyond the
-            validity of the statement.
-          </p>
-          <h3>2. What is R1CS?</h3>
-          <p>
-            Rank-1 Constraint System (R1CS) is a formulation used in zkSNARKs to express
-            computational problems as a set of constraints. These constraints define what
-            a valid solution looks like, enabling zkSNARKs to efficiently verify complex
-            computations.
-          </p>
-          <h3>3. Proof Systems:</h3>
-          <p>
-            <strong>Marlin:</strong> Marlin is a zkSNARK proof system designed for
-            universal and updatable recursive proofs. It optimizes proof size and
-            verification time, making it suitable for various applications requiring
-            scalability and efficiency.
-          </p>
-          <p>
-            <strong>Groth16:</strong> Groth16 is another zkSNARK proof system known for
-            its succinctness and efficiency in proving knowledge of a satisfying
-            assignment to a given NP statement. It is widely used in applications
-            requiring compact proofs and fast verification.
-          </p>         
-        </div>
+            <h3>What is a SNARK?</h3>
+            <p>
+              A succinct non-interactive argument of knowledge (SNARK) allows an untrusted
+              prover P to prove the knowledge of a witness w satisfying some property. For
+              example, w could be a pre-image of a designated value y of a cryptographic
+              hash function h, i.e., a w such that h(w) = y. A trivial proof is for P to
+              send w to the verifier V, who directly checks that w satisfies the claimed
+              property. A SNARK achieves the same, but with better verification costs (and
+              proof sizes). Succinct means that verifying a proof is exponentially faster
+              than checking the witness directly (this also implies that proofs are
+              exponentially smaller than the size of the statement proven).
+            </p>
+          </div>
         </header>
         <main className={styles.main}>
           <div className={styles.row}>
             <div className={styles.card}>
               <h2>Frameworks</h2>
               <p>
-            <a href='https://arkworks.rs/' target='_blank' rel='noopener noreferrer'>
-              arkworks.rs
-            </a>{" "}
-            is a Rust-based library providing essential tools and primitives for building
-            zkSNARK circuits and proofs. It supports various elliptic curves and
-            cryptographic primitives necessary for implementing zkSNARK protocols,
-            including circuit synthesizers for R1CS.
-          </p>
-          <p>
-            <a
-              href='https://github.com/succinctlabs/sp1'
-              target='_blank'
-              rel='noopener noreferrer'
-            > 
-              SP1
-            </a>{" "}
-            SP1 is a performant, 100% open-source, contributor-friendly zero-knowledge
-            virtual machine (zkVM) that can prove the execution of arbitrary Rust (or any
-            LLVM-compiled language) programs. SP1 democratizes access to ZKPs by allowing
-            developers to use programmable truth with popular programming languages. SP1
-            is inspired by the open-source software movement and takes a collaborative
-            approach towards building the best zkVM for rollups, coprocessors and other
-            ZKP applications. We envision a diversity of contributors integrating the
-            latest ZK innovations, creating a zkVM that is performant, customizable and
-            will stand the test of time.
-          </p>
-          <p>
-            By combining these concepts and technologies, our project demonstrates the
-            power and utility of zero-knowledge proofs in verifying computations while
-            maintaining data privacy and integrity.
-          </p>
+                <a href='https://arkworks.rs/' target='_blank' rel='noopener noreferrer'>
+                  arkworks.rs
+                </a>{" "}
+                is a Rust-based library providing essential tools and primitives for
+                building zkSNARK circuits and proofs. It supports various elliptic curves
+                and cryptographic primitives necessary for implementing zkSNARK protocols,
+                including circuit synthesizers for R1CS.
+              </p>
+              <p>
+                <a
+                  href='https://github.com/succinctlabs/sp1'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  SP1
+                </a>{" "}
+                SP1 is a performant, 100% open-source, contributor-friendly zero-knowledge
+                virtual machine (zkVM) that can prove the execution of arbitrary Rust (or
+                any LLVM-compiled language) programs. SP1 democratizes access to ZKPs by
+                allowing developers to use programmable truth with popular programming
+                languages. SP1 is inspired by the open-source software movement and takes
+                a collaborative approach towards building the best zkVM for rollups,
+                coprocessors and other ZKP applications. We envision a diversity of
+                contributors integrating the latest ZK innovations, creating a zkVM that
+                is performant, customizable and will stand the test of time.
+              </p>
             </div>
             <div className={styles.card}>
-              <h2>Supabase Next.js App Router Example</h2>
+              <h2>R1CS</h2>
+
               <p>
-                Want to see a code example containing some common patterns with Next.js and Supabase? Check out this repo!
+                Rank-1 Constraint System (R1CS) is a formulation used in zkSNARKs to
+                express computational problems as a set of constraints. These constraints
+                define what a valid solution looks like, enabling zkSNARKs to efficiently
+                verify complex computations.
+              </p>
+              <p>
+                So the R1CS protocol is a powerful way to formally capture an algebraic
+                circuit and to translate into a set of matrices and vectors that can be
+                fed into a downstream proof system
               </p>
             </div>
           </div>
           <div className={styles.row}>
             <div className={styles.card}>
-              <h2>Supabase Auth Helpers Docs</h2>
+              <h2>Cryptographic Primitives: </h2>
               <p>
-                This template has configured Supabase Auth to use cookies for you, but the docs are a great place to learn more.
+                Utilizing the Poseidon sponge hash function for matrix multiplication and
+                SHA-256 for the primality test, ensuring robust security and efficiency in
+                our proofs.
               </p>
             </div>
+
             <div className={styles.card}>
-              <h2>Next.js Front-end for zkProofs</h2>
+              <h2>zkSNARK Proof Systems</h2>
               <p>
-                The Next.js web front-end provides an intuitive interface for users to
-                interact with these zkProofs, demonstrating the practical application of
-                cryptographic protocols in a user-friendly environment.
+                <strong>Groth16:</strong> Groth16 is another zkSNARK proof system known
+                for its succinctness and efficiency in proving knowledge of a satisfying
+                assignment to a given NP statement. It is widely used in applications
+                requiring compact proofs and fast verification.
+              </p>
+              <p>
+                <strong>Marlin:</strong> Marlin is a zkSNARK proof system designed for
+                universal and updatable recursive proofs. It optimizes proof size and
+                verification time, making it suitable for various applications requiring
+                scalability and efficiency.
               </p>
             </div>
           </div>
