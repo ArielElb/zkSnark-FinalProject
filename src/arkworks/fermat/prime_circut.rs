31a23050dd3ad79d2dd89d25ae31470ec1917649
arie1 <ariel123elbaz@gmail.com>
2024-07-14 20:48:06 +0300

    added constraint for fermat circut

--- src/arkworks/fermat/prime_circut.rs
+++ src/arkworks/fermat/prime_circut.rs
@@ -22,6 +22,7 @@ use num_bigint::{BigUint, ToBigInt, ToBigUint};
 use std::{char::from_u32, ops::MulAssign};
 const NUM_BITS: usize = 381;
 
+use super::constraints::{fermat_circuit, fermat_constructor};
 use super::modulo;
 use super::shatry;
 
@@ -34,7 +35,8 @@ pub struct PrimeCheck<ConstraintF: PrimeField> {
     a_j_s: Vec<Vec<u8>>, // a vector of a_j = hash(x+j) for j in 0..i -1 // public input - to check that we actually calculated the hash correctly
     a_i: Vec<u8>,        // a_i = hash(x+i) // public input
     is_prime: bool,      // witness if the number is prime
-                         // modpow_ver_circuit: Vec<modpow_ver_circuit<ConstraintF>>, // vector of modpow circuits for each modpow.
+    // modpow_ver_circuit: Vec<modpow_ver_circuit<ConstraintF>>, // vector of modpow circuits for each modpow.
+    fermat_circuit: fermat_circuit<ConstraintF>,
 }
 // implement the constraints for the circuit:
 impl<ConstraintF: PrimeField> ConstraintSynthesizer<ConstraintF> for PrimeCheck<ConstraintF> {
@@ -47,11 +49,10 @@ impl<ConstraintF: PrimeField> ConstraintSynthesizer<ConstraintF> for PrimeCheck<
         let i_var = FpVar::<ConstraintF>::new_input(ark_relations::ns!(cs, "i"), || {
             Ok(ConstraintF::from(self.i))
         })?;
-        let r_var = FpVar::<ConstraintF>::new_input(ark_relations::ns!(cs, "r"), || Ok(self.r))?;
+        let r_var = FpVar::<ConstraintF>::new_witness(ark_relations::ns!(cs, "r"), || Ok(self.r))?;
         // create the witness:
         let is_prime_var =
             Boolean::new_witness(ark_relations::ns!(cs, "is_prime"), || Ok(self.is_prime))?;
-
         // for each j in 0..i-1:
         let mut sha256_var = Sha256Gadget::default();
         for j in 0..self.i {
@@ -59,7 +60,6 @@ impl<ConstraintF: PrimeField> ConstraintSynthesizer<ConstraintF> for PrimeCheck<
             let x_plus_j = self.x + ConstraintF::from(j);
             // convert x_plus_j to bytes:
             let x_plus_j_bytes = x_plus_j.into_bigint().to_bytes_le();
-
             // calculate the hash(x+j):
             sha256_var
                 .update(&shatry::to_byte_vars(ns!(cs, "input"), &x_plus_j_bytes))
@@ -74,7 +74,6 @@ impl<ConstraintF: PrimeField> ConstraintSynthesizer<ConstraintF> for PrimeCheck<
             println!("a_j_var: {:?}", a_j_var.value().unwrap());
             a_j_var.enforce_equal(&calculated_a_j)?;
         }
-
         // compute x+i:
         let x_plus_i = self.x + ConstraintF::from(self.i);
         // convert x_plus_i to bytes:
@@ -84,11 +83,49 @@ impl<ConstraintF: PrimeField> ConstraintSynthesizer<ConstraintF> for PrimeCheck<
         sha256_var
             .update(&shatry::to_byte_vars(ns!(cs, "input"), &x_plus_i_bytes))
             .unwrap();
-        let calculated_a_i = sha256_var.finalize().unwrap();
+        let calculated_a_i: DigestVar<ConstraintF> = sha256_var.finalize().unwrap();
         // enforce that a_i = hash(x+i):
         let a_i_var = DigestVar::new_input(ark_relations::ns!(cs, "a_i"), || Ok(self.a_i))?;
         a_i_var.enforce_equal(&calculated_a_i)?;
+
+        // let a_i_bytes: Vec<UInt8<ConstraintF>> = a_i_var.to_bytes().unwrap();
+
         println!("calculated_a_i: {:?}", calculated_a_i.value().unwrap());
+        // TODO: fermat primality
+        // TODO : validate that what i calculated is what in fermat_circuit.
+
+        let a_i_fpvar =
+            FpVar::<ConstraintF>::new_witness(ark_relations::ns!(cs, "a_i_fpvar"), || {
+                Ok(ConstraintF::from_le_bytes_mod_order(
+                    &a_i_var.to_bytes().unwrap().value().unwrap(),
+                ))
+            })?;
+        // create a witness from fermat_circuit:
+        let randomness_var_fermat = FpVar::<ConstraintF>::new_witness(
+            ark_relations::ns!(cs, "randomness_var_fermat"),
+            || Ok(self.fermat_circuit.a),
+        )?;
+        let n_var_fermat =
+            FpVar::<ConstraintF>::new_witness(ark_relations::ns!(cs, "n_var_fermat"), || {
+                Ok(self.fermat_circuit.n)
+            })?;
+
+        let is_prime_var_fermat =
+            Boolean::new_witness(ark_relations::ns!(cs, "is_prime_var_fermat"), || {
+                Ok(self.fermat_circuit.is_prime)
+            })?;
+
+        // enforce that the randomness is the same:
+        randomness_var_fermat.enforce_equal(&r_var)?;
+        // enforce that the n is the same:
+        n_var_fermat.enforce_equal(&a_i_fpvar)?;
+        // enforce that the is_prime is the same:
+        is_prime_var_fermat.enforce_equal(&is_prime_var)?;
+        // In the end create the constraints for the fermat circuit:
+        self.fermat_circuit
+            .generate_constraints(cs.clone())
+            .unwrap();
+
         Ok(())
     }
 }
@@ -119,6 +156,7 @@ mod tests {
     fn finalize(sha256: Sha256) -> Vec<u8> {
         sha256.finalize().to_vec()
     }
+
     #[test]
     fn initial_procces() {
         let mut rng = ark_std::test_rng();
@@ -154,6 +192,9 @@ mod tests {
         // do the hash for x+i:
         sha256.update(&x_plus_i_bytes);
         let a_i = finalize(sha256.clone());
+
+        // convert a_i to biguint:
+        let a_i_biguint: BigUint = BigUint::from_bytes_le(&a_i);
         println!("a_i: {:?}", a_i);
 
         // r = hash(x + i || a_i = hash(x+i) || i )
@@ -162,7 +203,10 @@ mod tests {
         sha256.update(&i.to_le_bytes());
         let r = finalize(sha256.clone());
         // convert r to Fr:
-        let r = Fr::from_be_bytes_mod_order(&r);
+        let r = Fr::from_le_bytes_mod_order(&r);
+
+        // create fermat circuit:
+        let fermat_circuit = fermat_constructor::<Fr>(a_i_biguint, BigUint::from(r));
 
         // create the circuit:
         let circuit = PrimeCheck {
@@ -172,6 +216,7 @@ mod tests {
             a_j_s: a_j_s.clone(),
             a_i,
             is_prime: false,
+            fermat_circuit,
         };
         circuit.generate_constraints(cs.clone()).unwrap();
         // check if the circuit is satisfied:
