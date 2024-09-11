b69f54faec37d9f7513cd3814351695afe691d24
ofekBinder <ofekBinder@gmail.com>
2024-09-11 12:26:04 +0300

    vfvf

--- bench/prime.py
+++ bench/prime.py
@@ -12,7 +12,7 @@ def generate_matrix(n):
 
 
 # רשימת הגדלים של המטריצות שנרצה לבדוק
-number_of_rounds =list(range(1, 50, 1)) #set the size of power of 2 for the matrixes you want
+number_of_rounds =list(range(2, 32, 1)) #set the size of power of 2 for the matrixes you want
 num_constraints = []
 proof_gen_time = []
 proof_set_up_time = []
@@ -21,46 +21,47 @@ proof_verif_time = []
 j_table = []
 try:
     for rounds in number_of_rounds:
-        random_32_bit_number = random.randint(0, 2 ** 32 - 1)
-        power_of_2_size=pow(2, rounds) #we calculate with powers of 2
-        print(f"Processing round: {rounds}")
-        print(f"Processing the round in power of 2: {power_of_2_size}")
-        print(random_32_bit_number)
-        print(power_of_2_size)
-
-        prove_input = {
-            'x': random_32_bit_number, #Seed number (x)
-            'i': power_of_2_size, #Number of rounds (i)
-        }
-
-        # שלח בקשה לשרת ליצירת הוכחה
-        response = requests.post(f'{base_url}/prime_arkworks/prove', json=prove_input)
-
-        if response.status_code == 200:
-            prove_output = response.json()
-            num_constraints.append(prove_output['num_constraints'])
-            proof_gen_time.append(prove_output['proving_time'])
-            proof_set_up_time.append(prove_output['setup_time'])
-            num_variables.append(prove_output['num_variables'])
-            j_table.append(prove_output['j'])
-            # שלח בקשה לשרת לאימות הוכחה
-            verify_input = {
-                'x': random_32_bit_number, #Seed number (x)
-                'j': prove_output['j'],
-                'proof': prove_output['proof'],
-                'pvk': prove_output['pvk'],
+        while True:
+            random_i_bit_number = random.randint(0, 2**rounds - 1)
+            print(f"Processing round: {rounds}")
+            print(f"Processing number: {random_i_bit_number}")
+            prove_input = {
+                'x': random_i_bit_number, #Seed number (x)
+                'i': 32, #Number of rounds (i)
             }
 
-            response = requests.post(f'{base_url}/prime_arkworks/verify', json=verify_input)
+            # שלח בקשה לשרת ליצירת הוכחה
+            response = requests.post(f'{base_url}/prime_arkworks/prove', json=prove_input)
 
             if response.status_code == 200:
-                verify_output = response.json()
-                proof_verif_time.append(verify_output['verifying_time'])
+                prove_output = response.json()
+                print(prove_output)
+                if prove_output['found_prime'] == False:
+                    continue
+                num_constraints.append(prove_output['num_constraints'])
+                proof_gen_time.append(prove_output['proving_time'])
+                proof_set_up_time.append(prove_output['setup_time'])
+                num_variables.append(prove_output['num_variables'])
+                j_table.append(prove_output['j'])
+                # שלח בקשה לשרת לאימות הוכחה
+                verify_input = {
+                    'x': random_i_bit_number, #Seed number (x)
+                    'j': prove_output['j'],
+                    'proof': prove_output['proof'],
+                    'pvk': prove_output['pvk'],
+                }
+
+                response = requests.post(f'{base_url}/prime_arkworks/verify', json=verify_input)
+
+                if response.status_code == 200:
+                    verify_output = response.json()
+                    proof_verif_time.append(verify_output['verifying_time'])
+                    break
+                else:
+                    print("wrong2")
+                    proof_verif_time.append(0)
             else:
-                print("wrong2")
-                proof_verif_time.append(0)
-        else:
-            print("wrong1")
+                print("wrong1")
 
 except Exception as e:
     print(f"An error occurred: {e}")
@@ -84,7 +85,7 @@ finally:
         'j_table': j_table,
         'proof_verif_time': proof_verif_time
     }
-    with open('results2.json', 'w') as f:
+    with open('results3.json', 'w') as f:
         json.dump(results, f, indent=4)
 
     print("Finished processing all matrices and results saved to 'resultsMatrix.json'")
@@ -95,7 +96,7 @@ finally:
     # גרף 1: כמות הסיבובים מול מספר הקונסטריינס
     plt.figure(figsize=(10, 7))
     plt.plot(number_of_rounds, num_constraints, marker='o')
-    plt.xlabel('number of rounds in power of 2')
+    plt.xlabel('the number of bits of the number')
     plt.ylabel('Circuit Size (# of constraints)')
     plt.title('Circuit Size vs. number of rounds')
     plt.xticks(rotation=45)
@@ -105,7 +106,7 @@ finally:
     # גרף 2: כמות הסיבובים מול מספר המשתנים
     plt.figure(figsize=(10, 7))
     plt.plot(number_of_rounds, num_variables, marker='o', color='green')
-    plt.xlabel('number of rounds in power of 2')
+    plt.xlabel('the number of bits of the number')
     plt.ylabel('number of variables')
     plt.title('number of variables vs. number of rounds')
     plt.xticks(rotation=45)
@@ -114,7 +115,7 @@ finally:
     # גרף 3: כמות הסיבובים מול זמן הsetup
     plt.figure(figsize=(10, 7))
     plt.plot(number_of_rounds, proof_set_up_time, marker='o', color='green')
-    plt.xlabel('number of rounds in power of 2')
+    plt.xlabel('the number of bits of the number')
     plt.ylabel('Proof  set up Time (seconds)')
     plt.title('Proof Generation set up  Time vs. number of rounds ')
     plt.xticks(rotation=45)
@@ -123,7 +124,7 @@ finally:
     # גרף 4: כמות הסיבובים מול זמן ההוכחה
     plt.figure(figsize=(10, 7))
     plt.plot(number_of_rounds, proof_gen_time, marker='o', color='green')
-    plt.xlabel('number of rounds in power of 2')
+    plt.xlabel('the number of bits of the number')
     plt.ylabel('Proof Generation Time (seconds)')
     plt.title('Proof Generation Time vs. number of rounds ')
     plt.xticks(rotation=45)
@@ -132,16 +133,16 @@ finally:
     # גרף 5: כמות הסיבובים מול j
     plt.figure(figsize=(10, 7))
     plt.plot(number_of_rounds, j_table, marker='o', color='green')
-    plt.xlabel('number of rounds in power of 2')
-    plt.ylabel('prime_round')
+    plt.xlabel('the number of bits of the number')
+    plt.ylabel('the number of rounds we use')
     plt.title('prime_round Time vs. number of rounds')
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.show()
-    # גרף 3: זמן אימות הוכחה מול כמות הסיבובים
+    # גרף 6: זמן אימות הוכחה מול כמות הסיבובים
     plt.figure(figsize=(10, 7))
     plt.plot(number_of_rounds, proof_verif_time, marker='o', color='red')
-    plt.xlabel('number of rounds in power of 2')
+    plt.xlabel('the number of bits of the number')
     plt.ylabel('Proof Verification Time (seconds)')
     plt.title('Proof Verification Time vs. Matrix Size')
     plt.xticks(rotation=45)
